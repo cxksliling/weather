d8b9415b00d49766f10bd2e7397a75d89f9156ea
Liling <cxksliling@gmail.com>
2017-04-12 20:52:12 +0800

    创建数据库weather，创建表Province、City、County

---
+++ app/src/main/java/com/cxksliling/weather/db/Province.java
@@ -0,0 +1,40 @@
+package com.cxksliling.weather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 26958 on 2017/4/12.
+ */
+
+public class Province extends DataSupport {
+
+    private int id;
+
+    private String provinceName;
+
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+}
